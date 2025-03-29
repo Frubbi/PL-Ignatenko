2396c438dbd39adb6f93ccd73630b1cdd41756c8
Frubbi <darinaign@mail.ru>
2025-03-29 19:48:14 +0300

    Update Main.java

--- java/pr_1/z_2/Main.java
+++ java/pr_1/z_2/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     
-    public static void second(String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите имя ");
         String name = scanner.nextLine();
