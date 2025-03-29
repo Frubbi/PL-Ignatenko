e11289faa2b82dcc67a7cdc2d606db5d799c6ea3
Frubbi <darinaign@mail.ru>
2025-03-29 19:56:11 +0300

    Update Main.java

--- java/pr_1/z_8/Main.java
+++ java/pr_1/z_8/Main.java
@@ -5,13 +5,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
         System.out.print("Введите строку символов: ");
         String input = scanner.nextLine();
-
         int upperCaseCount = 0;
         int lowerCaseCount = 0;
-
         for (char ch : input.toCharArray()) {
             if (Character.isUpperCase(ch)) {
                 upperCaseCount++;
@@ -19,7 +16,6 @@ public class Main {
                 lowerCaseCount++;
             }
         }
-
         String result;
         if (upperCaseCount > lowerCaseCount) {
             result = input.toUpperCase();
@@ -28,7 +24,6 @@ public class Main {
         } else {
             result = input.toLowerCase();
         }
-
         System.out.println("Результат: " + result);
     }
 }
