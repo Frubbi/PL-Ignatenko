a6407c098735ba4114cbd95ecdbeb0a62e7ee13c
Frubbi <darinaign@mail.ru>
2025-03-29 19:08:24 +0300

    Create Main.java

---
+++ java/pr_1/z_2/Main.java
@@ -0,0 +1,13 @@
+//    задание 2
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void second(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите имя ");
+        String name = scanner.nextLine();
+        String formattedName = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
+        System.out.println("Привет " + formattedName);
+    }
+}
