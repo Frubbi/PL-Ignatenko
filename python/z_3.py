fc99e67fe307ce594a6ae9002216881d7e044c7b
Frubbi <124764592+Frubbi@users.noreply.github.com>
2026-02-09 09:38:43 +0300

    Delete python directory

--- python/z_3.py
+++
@@ -1,9 +0,0 @@
-def check(y):
-    if(y % 4 == 0 and y % 100!=0) or y % 400 == 0:
-        return ('Да')
-    else:
-        return ('Нет')
-    
-y = int(input('Введите год, чтобы узнать високосный он или нет: '))
-
-print (check(y))
--- python/z_4.py
+++
@@ -1,9 +0,0 @@
-n=int(input('Введите число, чтобы рассчитать его факториал: '))
-
-def factorial(n):
-    result = 1
-    for i in range(1, n + 1):
-        result *= i
-    return result
-
-print(factorial(n))
